9aedec9d8bf58b4f8ac04a3731082756b89f51c6
kretzscd <43454638+kretzscd@users.noreply.github.com>
2019-02-14 11:06:33 -0700

    test_dump.sql has function includes and characters.html with new changes

--- test_dump.sql
+++ test_dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.16  Distrib 10.1.36-MariaDB, for Linux (x86_64)
 --
--- Host: classmysql.engr.oregonstate.edu    Database: cs340_lamartid
+-- Host: classmysql.engr.oregonstate.edu    Database: cs340_kretzscd
 -- ------------------------------------------------------
 -- Server version	10.1.22-MariaDB
 
@@ -16,37 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Character`
+-- Dumping routines for database 'cs340_kretzscd'
 --
-
-DROP TABLE IF EXISTS `Character`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Character` (
-  `character_id` int(11) NOT NULL AUTO_INCREMENT,
-  `character_name` varchar(100) NOT NULL,
-  `real_first_name` varchar(100) DEFAULT NULL,
-  `real_last_name` varchar(100) DEFAULT NULL,
-  `city` int(11) DEFAULT NULL,
-  `role` tinyint(1) NOT NULL,
-  `mentor_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`character_id`),
-  UNIQUE KEY `mentor_id` (`mentor_id`),
-  KEY `fk_Character_City` (`city`),
-  CONSTRAINT `fk_Character_Character` FOREIGN KEY (`mentor_id`) REFERENCES `Character` (`character_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_Character_City` FOREIGN KEY (`city`) REFERENCES `City` (`city_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Character`
---
-
-LOCK TABLES `Character` WRITE;
-/*!40000 ALTER TABLE `Character` DISABLE KEYS */;
-INSERT INTO `Character` VALUES (1,'Batman','Bruce','Wayne',1,1,NULL),(2,'Robin','Dick','Grayson',1,1,1),(3,'Green Arrow','Oliver','Queen',5,1,NULL),(4,'Brick','Daniel','Brickwell',5,0,NULL),(5,'Loki',NULL,NULL,NULL,0,NULL),(6,'Thor',NULL,NULL,NULL,1,NULL),(7,'Wonder Woman','Diana','Prince',NULL,1,NULL),(8,'Lex Luthor',NULL,NULL,2,0,NULL),(9,'Superman','Clark','Kent',1,1,NULL),(10,'The Joker','Jack','Napier',1,0,NULL),(11,'Bob the Goon',NULL,NULL,1,0,10),(12,'Wolferine','James','Howlett',NULL,1,NULL),(13,'Dare Devil','Matt','Murdock',3,1,NULL);
-/*!40000 ALTER TABLE `Character` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!50003 DROP FUNCTION IF EXISTS `mentorRole` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`cs340_kretzscd`@`%` FUNCTION `mentorRole`(m INT) RETURNS tinyint(1)
+BEGIN
+	DECLARE result1 BOOLEAN;
+    SELECT role INTO result1 FROM `Character` WHERE character_id = m;
+	RETURN result1;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `City`
@@ -74,32 +66,36 @@ INSERT INTO `City` VALUES (1,'Gotham City',0),(2,'Metropolis',0),(3,'New York Ci
 UNLOCK TABLES;
 
 --
--- Table structure for table `Equipment`
+-- Table structure for table `Character`
 --
 
-DROP TABLE IF EXISTS `Equipment`;
+DROP TABLE IF EXISTS `Character`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Equipment` (
-  `equipment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `equipment_name` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `material` varchar(100) DEFAULT NULL,
-  `character_id` int(11) NOT NULL,
-  PRIMARY KEY (`equipment_id`),
-  KEY `fk_Equipment_Character` (`character_id`),
-  CONSTRAINT `fk_Equipment_Character` FOREIGN KEY (`character_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE `Character` (
+  `character_id` int(11) NOT NULL AUTO_INCREMENT,
+  `character_name` varchar(100) NOT NULL,
+  `real_first_name` varchar(100) DEFAULT NULL,
+  `real_last_name` varchar(100) DEFAULT NULL,
+  `city` int(11) DEFAULT NULL,
+  `role` tinyint(1) NOT NULL,
+  `mentor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`character_id`),
+  KEY `fk_Character_City` (`city`),
+  KEY `fk_Character_Character` (`mentor_id`),
+  CONSTRAINT `fk_Character_Character` FOREIGN KEY (`mentor_id`) REFERENCES `Character` (`character_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_Character_City` FOREIGN KEY (`city`) REFERENCES `City` (`city_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Equipment`
+-- Dumping data for table `Character`
 --
 
-LOCK TABLES `Equipment` WRITE;
-/*!40000 ALTER TABLE `Equipment` DISABLE KEYS */;
-INSERT INTO `Equipment` VALUES (1,'Batmobile','Batman\'s heavily armored and weaponized car','steel',1),(2,'Truth Lasso','Lasso makes people tell the truth','Gold',7),(3,'Thor\'s Hammer','Hammer','Iron',6),(4,'Wolverine\'s Skeleton','Endoskeleton','Adamantium',12);
-/*!40000 ALTER TABLE `Equipment` ENABLE KEYS */;
+LOCK TABLES `Character` WRITE;
+/*!40000 ALTER TABLE `Character` DISABLE KEYS */;
+INSERT INTO `Character` VALUES (1,'Batman','Bruce','Wayne',1,1,NULL),(2,'Robin','Dick','Grayson',1,1,1),(3,'Green Arrow','Oliver','Queen',5,1,NULL),(4,'Brick','Daniel','Brickwell',5,0,NULL),(5,'Loki',NULL,NULL,NULL,0,NULL),(6,'Thor',NULL,NULL,NULL,1,NULL),(7,'Wonder Woman','Diana','Prince',NULL,1,NULL),(8,'Lex Luthor',NULL,NULL,2,0,NULL),(9,'Superman','Clark','Kent',1,1,NULL),(10,'The Joker','Jack','Napier',1,0,NULL),(11,'Bob the Goon',NULL,NULL,1,0,10),(12,'Wolferine','James','Howlett',NULL,1,NULL),(13,'Dare Devil','Matt','Murdock',3,1,NULL);
+/*!40000 ALTER TABLE `Character` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -150,6 +146,35 @@ LOCK TABLES `Weakness` WRITE;
 /*!40000 ALTER TABLE `Weakness` DISABLE KEYS */;
 INSERT INTO `Weakness` VALUES (1,'Kryptonite',10),(2,'Wonder Woman\'s Lasso',10),(3,'Thor letting go of Hammer',8),(4,'Muramasa Blade',10),(5,'Noise Pollution',8);
 /*!40000 ALTER TABLE `Weakness` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Equipment`
+--
+
+DROP TABLE IF EXISTS `Equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Equipment` (
+  `equipment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `equipment_name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `material` varchar(100) DEFAULT NULL,
+  `character_id` int(11) NOT NULL,
+  PRIMARY KEY (`equipment_id`),
+  KEY `fk_Equipment_Character` (`character_id`),
+  CONSTRAINT `fk_Equipment_Character` FOREIGN KEY (`character_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Equipment`
+--
+
+LOCK TABLES `Equipment` WRITE;
+/*!40000 ALTER TABLE `Equipment` DISABLE KEYS */;
+INSERT INTO `Equipment` VALUES (1,'Batmobile','Batman\'s heavily armored and weaponized car','steel',1),(2,'Truth Lasso','Lasso makes people tell the truth','Gold',7),(3,'Thor\'s Hammer','Hammer','Iron',6),(4,'Wolverine\'s Skeleton','Endoskeleton','Adamantium',12);
+/*!40000 ALTER TABLE `Equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -211,35 +236,6 @@ INSERT INTO `character_weaknesses` VALUES (1,1,9),(2,2,7),(3,3,6),(4,5,13);
 UNLOCK TABLES;
 
 --
--- Table structure for table `friend_relationship`
---
-
-DROP TABLE IF EXISTS `friend_relationship`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `friend_relationship` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `friend1_id` int(11) NOT NULL,
-  `friend2_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_friend_relationship_Character` (`friend1_id`),
-  KEY `fk_friend_relationship1_Character` (`friend2_id`),
-  CONSTRAINT `fk_friend_relationship1_Character` FOREIGN KEY (`friend2_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_friend_relationship_Character` FOREIGN KEY (`friend1_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `friend_relationship`
---
-
-LOCK TABLES `friend_relationship` WRITE;
-/*!40000 ALTER TABLE `friend_relationship` DISABLE KEYS */;
-INSERT INTO `friend_relationship` VALUES (1,6,7),(2,1,9),(3,10,11);
-/*!40000 ALTER TABLE `friend_relationship` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `rival_relationship`
 --
 
@@ -267,6 +263,35 @@ LOCK TABLES `rival_relationship` WRITE;
 INSERT INTO `rival_relationship` VALUES (1,5,6),(2,8,9),(3,1,10);
 /*!40000 ALTER TABLE `rival_relationship` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `friend_relationship`
+--
+
+DROP TABLE IF EXISTS `friend_relationship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friend_relationship` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `friend1_id` int(11) NOT NULL,
+  `friend2_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_friend_relationship_Character` (`friend1_id`),
+  KEY `fk_friend_relationship1_Character` (`friend2_id`),
+  CONSTRAINT `fk_friend_relationship1_Character` FOREIGN KEY (`friend2_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_friend_relationship_Character` FOREIGN KEY (`friend1_id`) REFERENCES `Character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friend_relationship`
+--
+
+LOCK TABLES `friend_relationship` WRITE;
+/*!40000 ALTER TABLE `friend_relationship` DISABLE KEYS */;
+INSERT INTO `friend_relationship` VALUES (1,6,7),(2,1,9),(3,10,11);
+/*!40000 ALTER TABLE `friend_relationship` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -277,4 +302,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-08 20:03:01
+-- Dump completed on 2019-02-14  9:22:47
