2bc37b3da98f84bc42f5ac21f72228f2960ed70b
kretzscd <kretzscd@oregonstate.edu>
2019-03-05 08:54:44 -0700

    Added validation

--- hero_db/public/script_powers_weakness.js
+++ hero_db/public/script_powers_weakness.js
@@ -13,6 +13,8 @@ function powerFunc(){
     var change = document.getElementById('toggle1');
     if(change.innerHTML == "Add a Power")
     {
+		document.getElementById('power_type').value = "";
+		document.getElementById('power_message').innerHTML = "";
         change.innerHTML = "Hide Form";
     }
     else{
@@ -27,7 +29,7 @@ function powerToCharacterFunc(){
     if(change.innerHTML == "Add a Power to a Character")
     {
         change.innerHTML = "Hide Form";
-            }
+    }
     else{
         change.innerHTML = "Add a Power to a Character";
         document.getElementById('addPowerToCharacter').style.display='none';
@@ -39,11 +41,14 @@ function weaknessFunc(){
     var change = document.getElementById('toggle3');
     if(change.innerHTML == "Add a Weakness")
     {
+		document.getElementById('weakness_type').value = "";
+		document.getElementById('message').innerHTML = "";
         change.innerHTML = "Hide Form";
     }
     else{
         change.innerHTML = "Add a Weakness";
         document.getElementById('addWeakness').style.display='none';
+		
     }
 }
 
@@ -58,4 +63,37 @@ function weaknessToCharacterFunc(){
         change.innerHTML = "Add a Weakness to a Character";
         document.getElementById('addWeaknessToCharacter').style.display='none';
     }
+}
+//reference = https://www.w3schools.com/js/js_validation.asp
+function validateNumber(){
+	var num, text;
+	text = "";
+	//document.getElementById("weakness_message").innerHTML = text;
+	//Get value from input field
+	num = document.forms["weakness_form"]["weakness_magnitude"].value;
+	if(num == null || num == '')
+	{
+		text = "The Weakness magnitude cannot be blank";
+		document.getElementById("message").innerHTML = text;
+		return false;
+	}
+	//set a message for the user in the form
+	document.getElementById("message").innerHTML = text;
+}
+
+//reference = https://www.w3schools.com/js/js_validation.asp
+function validatePowerNumber(){
+	var num, text;
+	text = "";
+	document.getElementById("power_message").innerHTML = text;
+	//Get value from input field
+	num = document.forms["power_form"]["power_magnitude"].value;
+	if(num == null || num == '')
+	{
+		text = "The Power magnitude cannot be blank";
+		document.getElementById("power_message").innerHTML = text;
+		return false;
+	}
+	//set a message for the user in the form
+	document.getElementById("power_message").innerHTML = text;
 }
